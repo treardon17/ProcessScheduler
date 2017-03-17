50863a74c337dc2838e2b03259fed0cc96de6e09
Tyler Reardon <tdr.reardon@gmail.com>
2017-03-16 20:46:58 -0700

    Added timer

--- PCB.py
+++ PCB.py
@@ -1,3 +1,5 @@
+import time
+
 #Class to define constant values for processor state
 class ProcessState:
     ready = 'READY'
@@ -8,7 +10,6 @@ class ProcessState:
 class PCB:
 
     def __init__ (self, ID, burst):
-
         #getting the PCB.ID from the .txt file
         self.ID = ID
         #getting the PCB.burst from the .txt file
@@ -21,6 +22,21 @@ class PCB:
         self.wait = 0
         self.response = 0
 
+        #the process is waiting
+        self.wait()
+
     #this should output string values of everything rather than pointer values
     def __str__ (self):
         return str(self.ID, self.burst, self.priority, self.accumulatedTime, self.wait, self.response)
+
+    def wait(self):
+        #set the state to ready because it's not running anymore (yet)
+        self.state = ProcessState.ready
+        #keep track of the time we start waiting
+        self.startTime = time.time()
+
+    def run(self):
+        #the process is now running
+        self.state = ProcessState.running
+        #get the current time it started running
+        self.accumulatedTime += (time.time() - self.startTime)
